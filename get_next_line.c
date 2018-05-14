eaec805cf53c086c27f4e96b89db6c1ddc33f12e
z10n <z10n.w3n.42@gmail.com>
2018-05-14 01:44:25 -0700

    make it work again

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 23:22:01 by zwen              #+#    #+#             */
-/*   Updated: 2018/05/13 21:23:19 by zwen             ###   ########.fr       */
+/*   Updated: 2018/05/14 01:06:26 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,46 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-/*
-static int	ft_get_line(char **str, char *nlptr, char *line)
-{
-	puts("2\n");
-	line = ft_strndup(*str, nlptr - *str);
-	ft_strdel(str);
-	*str = ft_strdup(nlptr + 1);
-	return (1);
-}
-*/
-
 int			get_next_line(int fd, char **line)
 {
-	static char	*str;
-	char		*buf;
+	static char	*str[1024];
+	char		buf[BUFF_SIZE + 1];
 	char		*nlptr;
+	int			len;
+	char		*tmp;
 
-	buf = ft_strnew(BUFF_SIZE);
 	if (fd == -1 || !line || read(fd, buf, 0) < 0)
 		return (-1);
-	if (!str)
-		str = ft_strnew(0);
-//	if ((nlptr = ft_strchr(str, '\n')))
-//	{
-//		puts("3\n");
-//		*line = ft_strndup(str, nlptr - str);
-//		ft_strdel(&str);
-//		str = ft_strdup(nlptr + 1);
-//		return (1);
-//	}
-	while (!(nlptr = ft_strchr(str, '\n')) && read(fd, buf, BUFF_SIZE))
+	if (!str[fd])
+		str[fd] = ft_strnew(0);
+	if ((nlptr = ft_strchr(str[fd], '\n')))
 	{
-		free(str);
-		str = ft_strjoin(str, buf);
-	//	if (ft_strchr(buf, '\n'))
-	//		break ;
-	}
-//	puts("1\n");
-//	if ((nlptr = ft_strchr(str, '\n')))
-//		return (ft_get_line(&str, nlptr, *line));
-	if (nlptr)
-	{	
-		*line = ft_strndup(str, nlptr - str);
-		ft_strdel(&str);
-		str = ft_strdup(nlptr + 1);
+		*line = ft_strndup(str[fd], nlptr - str[fd]);
+		tmp = str[fd];
+		str[fd] = ft_strdup(nlptr + 1);
+		ft_strdel(&tmp);
 		return (1);
 	}
-	*line = ft_strdup(str);
-	if (!ft_strlen(str))
+	while ((len = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[len] = '\0';
+		tmp = str[fd];
+		str[fd] = ft_strjoin(str[fd], buf);
+		free(tmp);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	if ((nlptr = ft_strchr(str[fd], '\n')))
+	{	
+		*line = ft_strndup(str[fd], nlptr - str[fd]);
+		tmp = str[fd];
+		str[fd] = ft_strdup(nlptr + 1);
+		ft_strdel(&tmp);
+		return (1);
+	}
+	*line = ft_strdup(str[fd]);
+	if (!str[fd] || !*str[fd])
 		return (0);
-	ft_strdel(&str);
+	ft_strdel(&str[fd]);
 	return (1);
 }
