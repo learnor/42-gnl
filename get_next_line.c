e6f3a466390c445a2194af8b493236fd41f33918
z10n <z10n.w3n.42@gmail.com>
2018-05-17 05:39:30 -0700

    21 lines

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 23:22:01 by zwen              #+#    #+#             */
-/*   Updated: 2018/05/17 05:32:31 by zwen             ###   ########.fr       */
+/*   Updated: 2018/05/17 05:05:23 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,25 @@
 
 int			get_next_line(int fd, char **line)
 {
-	static char	*str[1024];
+	static char	*str[65535];
 	char		buf[BUFF_SIZE + 1];
-	char		*nlptr;
+	char		*ptr;
 	int			len;
-	char		*tmp;
 
 	if (!line || read(fd, buf, 0) < 0)
 		return (-1);
-	if (!str[fd])
-	   str[fd] = ft_strnew(0);
+	str[fd] = (str[fd]) ? : ft_strnew(0);
 	while (!ft_strrchr(str[fd], '\n') && (len = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[len] = '\0';
-		tmp = str[fd];
-		str[fd] = ft_strjoin(str[fd], buf);
-		ft_strdel(&tmp);
+		str[fd] = ft_strjoin(ptr = str[fd], buf);
+		ft_strdel(&ptr);
 	}
-	if ((nlptr = ft_strchr(str[fd], '\n')))
-	{
-		*line = ft_strndup(str[fd], nlptr - str[fd]);
-		tmp = str[fd];
-		str[fd] = ft_strdup(nlptr + 1);
-		ft_strdel(&tmp);
-		return (1);
-	}
-	*line = ft_strdup(str[fd]);
 	if (!str[fd] || !*str[fd])
 		return (0);
-	ft_strdel(&str[fd]);
+	*line = str[fd];
+	str[fd] = (ptr = ft_strchr(str[fd], '\n')) ? ft_strdup(ptr + 1) : NULL;
+	if (ptr)
+		*ptr = '\0';
 	return (1);
 }
