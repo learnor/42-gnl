e3b10900dab9e869d7a0cc52d24613e64cb89b75
z10n <z10n.w3n.42@gmail.com>
2018-05-22 16:18:05 -0700

    fix norm double assignment and may not compile

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,12 @@
 /*   By: zwen <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 23:22:01 by zwen              #+#    #+#             */
-/*   Updated: 2018/05/17 05:05:23 by zwen             ###   ########.fr       */
+/*   Updated: 2018/05/22 16:17:22 by zwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "get_next_line.h"
-#include <stdio.h>
 
 int			get_next_line(int fd, char **line)
 {
@@ -23,17 +22,19 @@ int			get_next_line(int fd, char **line)
 
 	if (!line || read(fd, buf, 0) < 0)
 		return (-1);
-	str[fd] = (str[fd]) ? : ft_strnew(0);
+	str[fd] = str[fd] ? str[fd] : ft_strnew(0);
 	while (!ft_strrchr(str[fd], '\n') && (len = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[len] = '\0';
-		str[fd] = ft_strjoin(ptr = str[fd], buf);
+		ptr = str[fd];
+		str[fd] = ft_strjoin(ptr, buf);
 		ft_strdel(&ptr);
 	}
 	if (!str[fd] || !*str[fd])
 		return (0);
 	*line = str[fd];
-	str[fd] = (ptr = ft_strchr(str[fd], '\n')) ? ft_strdup(ptr + 1) : NULL;
+	ptr = ft_strchr(str[fd], '\n');
+	str[fd] = (ptr) ? ft_strdup(ptr + 1) : NULL;
 	if (ptr)
 		*ptr = '\0';
 	return (1);
